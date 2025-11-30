10d469b026d7c9da3e3576b3864bb36c4df302a7
KalvinAngelitoAng <kalvinangelitoang88@gmail.com>
2025-12-01 07:57:21 +0800

    Commit 5 : Mengupdate api.js dan juga config.js agar tidak hardcode dan bisa membacaq dotenv

--- frontend/src/pages/utils/api.js
+++ frontend/src/pages/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:3000/api/auth";
+const API_BASE = (import.meta.env.VITE_API_BASE || "http://localhost:3000/api/auth");
 import { BASE_URL } from "./config";
 import { getAccessToken } from "./auth";
 
--- frontend/src/pages/utils/config.js
+++ frontend/src/pages/utils/config.js
@@ -1,3 +1,5 @@
 export const ACCESS_TOKEN_KEY = "accessToken";
 
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = (import.meta.env.VITE_BASE_URL || "http://localhost:3000");
+
+
